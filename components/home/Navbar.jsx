d084f9d3ed03205d766c24ec85acb349174e9cc9
Mohith kumar <murugan28aug@gmail.com>
2023-01-24 16:10:14 +0530

    ui

--- components/home/Navbar.jsx
+++ components/home/Navbar.jsx
@@ -105,18 +105,30 @@ const Navbar = () => {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pt-2 pb-3">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={'text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+            <div className="flex flex-col space-y-1 px-2 pt-2 pb-3 ">
+            <button
+                        onClick={()=>{router.push("/")}}
+                        className={'text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'}
+                      >
+                        Home
+                      </button>
+                      <button
+                    
+                        onClick={()=>{router.push("/#services")}}
+                        className={'text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                        }
+                      >
+                        Services
+                      </button>
+                      <button
+                        onClick={()=>{router.push("/#contactUs")}}
+                        className={classNames(
+                          router.pathname.includes("/projects") ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                      >
+                        Contact Us
+                      </button>
             </div>
           </Disclosure.Panel>
         </>
